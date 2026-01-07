cf4d9be751c59c4eafb826e03652219d6f3b4b4c
MarcoAndersohn <marco.andersohn@gmail.com>
2026-01-07 02:35:30 +0100

    Sync am 07.01.2026 um  2:35:29,95

--- All_in_One_Pipeline.py
+++ All_in_One_Pipeline.py
@@ -199,7 +199,8 @@ def main():
                     
                 atoms = read(source_out, index=-1)
                 elements = sorted(list(set(atoms.get_chemical_symbols())))
-                pseudo_path = os.path.join(os.path.dirname(PW_EXE), "pseudo").replace("\\", "/") + "/"
+                %pseudo_path = os.path.join(os.path.dirname(PW_EXE), "pseudo").replace("\\", "/") + "/"
+                pseudo_path = os.path.join(WORK_DIR, "pseudo").replace("\\", "/") + "/"
                 
                 # VORBEREITUNG DER LISTEN (Fix für SyntaxError)
                 species_str = "".join([f" {el} 1.0 {el}.UPF\n" for el in elements])
